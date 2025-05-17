8fcd591f339d16b495ef1468680a673e6ee8e3e5
anushap976 <mail2anusha14@gmail.com>
2025-05-16 21:09:50 -0500

    Changes in application

--- user-profile-react-app/src/components/ProfileCard.jsx
+++ user-profile-react-app/src/components/ProfileCard.jsx
@@ -1,10 +1,10 @@
 
-const ProfileCard = ({ name, age, bio }) => {
+const ProfileCard = (props) => {
     return (
         <div className="profile-card">
-            <h3>Name: {name}</h3>
-            <p><strong>Age:</strong> {age}</p>
-            <p><strong>Bio:</strong> {bio}</p>
+            <h3>Name: {props.name}</h3>
+            <p><strong>Age:</strong> {props.age}</p>
+            <p><strong>Bio:</strong> {props.bio}</p>
         </div>
     );
 };
